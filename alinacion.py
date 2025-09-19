0571c9c326abe10826a9c620bd685d385f069c06
Topo Frank <florianfranklin@gmail.com>
2025-09-19 08:32:23 -0600

    Update alinacion.py

--- alinacion.py
+++ alinacion.py
@@ -78,9 +78,9 @@ ax.plot([proj[0]-0.5, proj[0]+0.5], [proj[1], proj[1]], 'g', linewidth=2)
 ax.plot([proj[0], proj[0]], [proj[1]-0.5, proj[1]+0.5], 'g', linewidth=2)
 
 # Desplazamiento de etiquetas
-offset = 1  # alejar etiquetas de los puntos
+offset = 3  # alejar etiquetas de los puntos
 ax.text(xPT + offset, yPT + offset, "PT", color='red', fontsize=8, fontweight='bold')
-ax.text(proj[0] + offset, proj[1] + offset, "Proy", color='green', fontsize=8, fontweight='bold')
+ax.text(proj[1] + offset, proj[1] + offset, "Proy", color='green', fontsize=8, fontweight='bold')
 
 # Distancia perpendicular
 mid_x = (xPT + proj[0]) / 2
@@ -105,6 +105,7 @@ ax.axis("equal")
 ax.legend(fontsize=9)
 
 st.pyplot(fig)
+
 
 
 
